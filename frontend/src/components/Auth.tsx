dddde068bcfb50a6696b7c80db4b461b6fcfdae7
Lan Laucirica <lan@primedeviation.com>
2025-06-13 15:45:50 -0700

    refactor(auth): simplify login to be passwordless magic link

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -4,27 +4,12 @@ import React, { useState } from 'react';
 const Auth: React.FC = () => {
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
       setLoading(true);
-      const { error } = await supabase.auth.signInWithPassword({ email, password });
-      if (error) throw error;
-      alert('Logged in successfully!');
-    } catch (error: any) {
-      alert(error.error_description || error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const handleSignup = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      setLoading(true);
-      const { error } = await supabase.auth.signUp({ email, password });
+      const { error } = await supabase.auth.signInWithOtp({ email });
       if (error) throw error;
       alert('Check your email for the login link!');
     } catch (error: any) {
@@ -37,7 +22,7 @@ const Auth: React.FC = () => {
   return (
     <div>
       <div>
-        <h1>Sign Up / Sign In</h1>
+        <h1>Employable</h1>
         <p>Sign in via magic link with your email below</p>
         <form onSubmit={handleLogin}>
           <label htmlFor="email">Email</label>
@@ -48,19 +33,8 @@ const Auth: React.FC = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            placeholder="Your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
           <button disabled={loading}>
-            {loading ? <span>Loading</span> : <span>Login</span>}
-          </button>
-          <button onClick={handleSignup} disabled={loading}>
-            {loading ? <span>Loading</span> : <span>Sign Up</span>}
+            {loading ? <span>Loading</span> : <span>Send magic link</span>}
           </button>
         </form>
       </div>
